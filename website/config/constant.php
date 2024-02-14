a97a0178bbfe56866805bbf5aaa4c159d57e17c8
web-project <adhikariprashant857@gmail.com>
2024-02-14 10:01:31 +0545

    Add folders to repository

---
+++ website/config/constant.php
@@ -0,0 +1,11 @@
+<?php
+//start session
+session_start();
+define('SITEURL','http://localhost/PRACTICE GIT/');
+define('LOCALHOST','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define("DB_DATABASE","handscrafts-order");
+$conn= mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$db_select=mysqli_select_db($conn,'handscrafts-order');
+?>
