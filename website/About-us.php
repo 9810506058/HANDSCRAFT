bb6a9980662931f050cb54532258eb22b2683731
web-project <adhikariprashant857@gmail.com>
2024-02-14 11:45:41 +0545

    changes

---
+++ website/About-us.php
@@ -0,0 +1,54 @@
+
+<?php include('partials-frontend/nav.php'); ?>
+
+<div class="container bg-light">
+    <h1>About  Us</h1>
+    <p>We are always ready to help you. Please feel free to contact us. We will get back to you as soon as we can.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, quis quia quos incidunt hic consequuntur? Obcaecati totam neque autem architecto. Dolorum, dolore. Nihil dolores libero repellendus mollitia nemo esse accusamus.</p>
+    <img src="https://picsum.photos/300" alt=""> 
+    <img src="https://picsum.photos/300" alt="">
+    <img src="https://picsum.photos/300" alt="">
+    <img src="https://picsum.photos/300" alt="">
+    
+    <br><br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
+    <!-- what people tell about us -->
+    <section id="testimonials">
+        <div class="container">
+            <h1 class="text-center">What People Tell About Us</h1>
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="https://picsum.photos/200" alt="" class="w-100">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="https://picsum.photos/200" alt="" class="w-100">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="https://picsum.photos/200" alt="" class="w-100">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</div>
+
+
+
+
+<?php include('partials-frontend/footer.php'); ?>
