e0c9c38ba427454631fb13ad666aae5462fde2a6
web-project <adhikariprashant857@gmail.com>
2024-02-14 10:24:29 +0545

    changed

--- website/config/constant.php
+++ website/config/constant.php
@@ -1,7 +1,7 @@
 <?php
 //start session
 session_start();
-define('SITEURL','http://localhost/PRACTICE GIT/');
+define('SITEURL','http://localhost/HANDSCRAFT/website/');
 define('LOCALHOST','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
