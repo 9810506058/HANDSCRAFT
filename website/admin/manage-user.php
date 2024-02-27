c6c421e5dacf01afa5fbeb7f8fd4575dcd476dc6
web-project <adhikariprashant857@gmail.com>
2024-02-28 05:41:05 +0545

    few changes

--- website/admin/manage-user.php
+++ website/admin/manage-user.php
@@ -1,102 +1,229 @@
-<?php
-    include('partials/menu.php');
-?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<div class="container">
-    <div class="wrapper">
-        <h1>Manage Users</h1>
-        <br>
-        <br>
-        <?php
-            if (isset($_SESSION['add'])) {
-                echo $_SESSION['add'];
-                unset($_SESSION['add']);
-            }
-            if (isset($_SESSION['delete'])) {
-                echo $_SESSION['delete'];
-                unset($_SESSION['delete']);
-            }
-            if (isset($_SESSION['update'])) {
-                echo $_SESSION['update'];
-                unset($_SESSION['update']);
-            }
-            if (isset($_SESSION['user-not-found'])) {
-                echo $_SESSION['user-not-found'];
-                unset($_SESSION['user-not-found']);
-            }
-            if (isset($_SESSION['pwd-not-match'])) {
-                echo $_SESSION['pwd-not-match'];
-                unset($_SESSION['pwd-not-match']);
-            }
-            if (isset($_SESSION['change-pwd'])) {
-                echo $_SESSION['change-pwd'];
-                unset($_SESSION['change-pwd']);
-            }
-        ?>
-        <br>
-        <br>
-        <a href="add-users.php" class="btn btn-primary">Add User</a>
-        <br>
-        <br>
-        <br>
-        <div class="table-responsive-sm">
-            <table class="table table-bordered">
-                <thead class="table-danger">
-                    <tr>
-                        <th>S.N</th>
-                        <th>Username</th>
-                        <th>Address</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Actions</th>
-                    </tr>   
-                </thead>
-                <tbody>
-                    <?php
-                        $sql = "SELECT * FROM tbl_users";
-                        $res = mysqli_query($conn, $sql);
 
-                        if ($res) {
-                            $sn = 1;
-                            while ($rows = mysqli_fetch_assoc($res)) {
-                                $id = $rows['id'];
-                                $username = $rows['username'];
-                                $address = $rows['address'];
-                                $email = $rows['email'];
-                                $userType = $rows['userType'];
-                    ?>
-                    <tr>
-                        <td><?php echo $sn++; ?></td>
-                        <td><?php echo $username; ?></td>
-                        <td><?php echo $address; ?></td>
-                        <td><?php echo $email; ?></td>
-                        <td><?php echo $userType; ?></td>
-                        <td>
-                            <a href="<?php echo SITEURL; ?>admin/update-user.php?id=<?php echo $id; ?>" class="btn btn-secondary">Update</a>
-                            <a href="#" onclick="confirmDelete('<?php echo SITEURL; ?>admin/delete-user.php?id=<?php echo $id; ?>');"> <i class="fa-solid fa-trash"></i></a>
-                                                        <a href="#" data-toggle="modal" data-target="#changePasswordModal<?php echo $id; ?>";> <i class="fa-solid fa-key"></i></a>
-                            <!-- Add modal for change password and delete modal -->
-                        </td>
-                    </tr>
-                    <?php
-                            }
-                        }
-                    ?>
-                </tbody>
-            </table>
+<?php include('partials/menu.php');
+
+?>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<div class="container-fluid" style="margin-top:98px">
+	
+	<div class="row">
+        <div class="col-lg-12">
+            <button class="btn btn-primary float-right btn-sm" data-toggle="modal" data-target="#newUser"><i class="fa fa-plus"></i> New user</button>
         </div>
-    </div>
+	</div>
+	    <br>
+	<div class="row">
+		<div class="card col-lg-12">
+			<div class="card-body">
+				<table class="table-striped table-bordered col-md-12 text-center">
+                    <thead style="background-color: rgb(111 202 203);">
+                        <tr>
+                            <th>UserId</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Phone No.</th>
+                            <th>Type</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT * FROM tbl_users"; 
+                            $result = mysqli_query($conn, $sql);
+                            
+                            while($row=mysqli_fetch_assoc($result)) {
+                                $Id = $row['id'];
+                                $username = $row['username'];
+                                $email = $row['email'];
+                                $phone = $row['phone'];
+                                $userType = $row['userType'];
+                                if($userType == 0) 
+                                    $userType = "user";
+                                else
+                                    $userType = "Admin";
+
+                                echo '<tr>
+                                    <td>' .$Id. '</td>
+                                    <td>' .$username. '</td>
+                                    
+                                    
+                                    <td>' .$email. '</td>
+                                    <td>' .$phone. '</td>
+                                    <td>' .$userType. '</td>
+                                    <td class="text-center">
+                                        <div class="row mx-auto" style="width:112px">
+                                            <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editUser' .$Id. '" type="button">Edit</button>';
+                                            if($Id == 1) {
+                                                echo '<button class="btn btn-sm btn-danger" disabled style="margin-left:9px;">Delete</button>';
+                                            }
+                                            else {
+                                                echo '<form action="_userManage.php" method="POST">
+                                                        <button name="removeUser" class="btn btn-sm btn-danger" style="margin-left:9px;">Delete</button>
+                                                        <input type="hidden" name="Id" value="'.$Id. '">
+                                                    </form>';
+                                            }
+
+                                    echo '</div>
+                                    </td>
+                                </tr>';
+                            }
+                        ?>
+                    </tbody>
+		        </table>
+			</div>
+		</div>
+	</div>
 </div>
 
-<script>
-    function confirmDelete(deleteUrl) {
-        var confirmDelete = confirm("Are you sure you want to delete this user?");
-        if (confirmDelete) {
-            window.location.href = deleteUrl;
-        }
-    }
-</script>
+<!-- newUser Modal -->
+<div class="modal fade" id="newUser" tabindex="-1" role="dialog" aria-labelledby="newUser" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color: rgb(111 202 203);">
+        <h5 class="modal-title" id="newUser">Create New User</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="_userManage.php" method="post">
+              <div class="form-group">
+                  <b><label for="username">Username</label></b>
+                  <input class="form-control" id="username" name="username" placeholder="Choose a unique Username" type="text" required minlength="3" maxlength="11">
+              </div>
+             
+              <div class="form-group">
+                  <b><label for="email">Email:</label></b>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="Enter Your Email" required>
+              </div>
+              <div class="form-group row my-0">
+                    <div class="form-group col-md-6 my-0">
+                        <b><label for="phone">Phone No:</label></b>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon">+91</span>
+                            </div>
+                            <input type="tel" class="form-control" id="phone" name="phone" placeholder="Enter Phone No" required pattern="[0-9]{10}" maxlength="10">
+                        </div>
+                    </div>
+                    <div class="form-group col-md-6 my-0">
+                        <b><label for="userType">Type:</label></b>
+                        <select name="userType" id="userType" class="custom-select browser-default" required>
+                        <option value="0">User</option>
+                        <option value="1">Admin</option>
+                        </select>
+                    </div>
+              </div>
+              <div class="form-group">
+                  <b><label for="password">Password:</label></b>
+                  <input class="form-control" id="password" name="password" placeholder="Enter Password" type="password" required data-toggle="password" minlength="4" maxlength="21">
+              </div>
+              
+              <button type="submit" name="createUser" class="btn btn-success">Submit</button>
+            </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php 
+    $usersql = "SELECT * FROM `tbl_users`";
+    $userResult = mysqli_query($conn, $usersql);
+    while($userRow = mysqli_fetch_assoc($userResult)){
+        $Id = $userRow['id'];
+        $name = $userRow['username'];
+       
+        $email = $userRow['email'];
+        $phone = $userRow['phone'];
+        $userType = $userRow['userType'];
+
+
+?>
+<!-- editUser Modal -->
+<div class="modal fade" id="editUser<?php echo $Id; ?>" tabindex="-1" role="dialog" aria-labelledby="editUser<?php echo $Id; ?>" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color: rgb(111 202 203);">
+        <h5 class="modal-title" id="editUser<?php echo $Id; ?>">User Id: <b><?php echo $Id; ?></b></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+        <div class="modal-body">
+            
+            <div class="text-left my-2 row" style="border-bottom: 2px solid #dee2e6;">
+                <div class="form-group col-md-8">
+                    <form action="_userManage.php" method="post" enctype="multipart/form-data">
+                        <b><label for="image">Profile Picture</label></b>
+                        <input type="file" name="userimage" id="userimage" accept=".jpg" class="form-control" required style="border:none;">
+                        <small id="Info" class="form-text text-muted mx-3">Please .jpg file upload.</small>
+                        <input type="hidden" id="userId" name="userId" value="<?php echo $Id; ?>">
+                        <button type="submit" class="btn btn-success mt-3" name="updateProfilePhoto">Update Img</button>
+                    </form>         
+                </div>
+                <div class="form-group col-md-4">
+                    <img src="/OnlinePizzaDelivery/img/person-<?php echo $Id; ?>.jpg" alt="Profile Photo" width="100" height="100" onError="this.src ='/OnlinePizzaDelivery/img/profilePic.jpg'">
+                    <form action="_userManage.php" method="post">
+                        <input type="hidden" id="userId" name="userId" value="<?php echo $Id; ?>">
+                        <button type="submit" class="btn btn-success mt-2" name="removeProfilePhoto">Remove Img</button>
+                    </form>
+                </div>
+            </div>
+            
+            <form action="_userManage.php" method="post">
+                <div class="form-group">
+                    <b><label for="username">Username</label></b>
+                    <input class="form-control" id="username" name="username" value="<?php echo $name; ?>" type="text" disabled>
+                </div>
+               
+                <div class="form-group">
+                    <b><label for="email">Email:</label></b>
+                    <input type="email" class="form-control" id="email" name="email" value="<?php echo $email; ?>" required>
+                </div>
+                <div class="form-group row my-0">
+                    <div class="form-group col-md-6 my-0">
+                        <b><label for="phone">Phone No:</label></b>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon">+91</span>
+                            </div>
+                            <input type="tel" class="form-control" id="phone" name="phone" value="<?php echo $phone; ?>" required pattern="[0-9]{10}" maxlength="10">
+                        </div>
+                    </div>
+                    <div class="form-group col-md-6 my-0">
+                        <b><label for="userType">Type:</label></b>
+                        <select name="userType" id="userType" class="custom-select browser-default" required>
+                        <?php 
+                            if($userType == 1) {
+                        ?>
+                            <option value="0">User</option>
+                            <option value="1" selected>Admin</option>
+                        <?php
+                            } 
+                            else {
+                        ?>
+                            <option value="0" selected>User</option>
+                            <option value="1">Admin</option>
+                        <?php
+                            } 
+                        ?>
+                        </select>
+                    </div>
+                </div>
+                <input type="hidden" id="userId" name="userId" value="<?php echo $Id; ?>">
+                <button type="submit" name="editUser" class="btn btn-success">Update</button>
+            </form>
+        </div>
+    </div>
+  </div>
+</div>
 
 <?php
-    include('partials/footer.php');
+    }
 ?>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
