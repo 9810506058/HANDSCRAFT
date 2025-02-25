7d528fc4f715df5e244919471308607571b07241
Prashant <adhikariprashant857@gmail.com>
2025-02-25 17:51:36 +0545

    new change

--- website/config/constant.php
+++ website/config/constant.php
@@ -5,7 +5,7 @@ define('SITEURL','http://localhost/HANDSCRAFT/website/');
 define('LOCALHOST','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
-define("DB_DATABASE","handscrafts-order");
+define("DB_DATABASE","handscraft-order");
 $conn= mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-$db_select=mysqli_select_db($conn,'handscrafts-order');
+$db_select=mysqli_select_db($conn,'handscraft-order');
 ?>
